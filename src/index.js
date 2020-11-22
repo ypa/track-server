107d48c700059e2b49f22fcd8dd28da909c2c47a
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-21 20:23:16 -0800

    Add connection to mongo cloud instance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+const mongoUri = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.mtymp.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
